6cb04751325504eeac86d8c6782fdaeb7817d26c
Máté <nohab2005@gmail.com>
2022-10-04 20:20:56 +0200

    finished navbars, final colors and added mobile view

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 function reveal() {
   var reveals = document.querySelectorAll(".reveal");
   for (var i = 0; i < reveals.length; i++) {
-    var windowHeight = window.innerHeight;
+    var pageHeight = window.innerHeight;
     var elementTop = reveals[i].getBoundingClientRect().top;
     var elementVisible = 150;
-    if (elementTop < windowHeight - elementVisible) {
+    if (elementTop < pageHeight - elementVisible) {
       reveals[i].classList.add("active");
     } else {
       reveals[i].classList.remove("active");
